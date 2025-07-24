d9f30e7b49fbe070aa0aa955df38dd881e800b58
Blair <mawubablair@gmail.com>
2025-07-24 10:08:47 +0300

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,17 @@
+# This code snippet demonstrate how to create a list OF dictionaries in python
+#and how to add anew dictionary to the list
+students =[
+    {"username": "B96227@students.ucu .ac.ug","password": "12345"}
+    {"username": "B96337@students.ucu .ac.ug","password": "12345"}
+    {"username": "B96228@students.ucu .ac.ug","password": "12345"}
+]
+
+student ={
+    "username": "B96227@students.ucu .ac.ug",
+    "password": "12345"
+}
+students.append(student)
+students.insert(1,student)
+
+
+    
